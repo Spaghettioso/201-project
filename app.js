55d110fee2526bbf69fd5937e10c5f56c07d9231
Jonathan OReilly <jonathanoreilly@live.co.uk>
2023-07-05 14:10:21 +0100

    basic page with click to continue text and incomplete form

---
+++ app.js
@@ -0,0 +1,28 @@
+"use strict";
+let paragraph1 =
+  "Hail traveller! Pull up a stool and warm yourself by the fire";
+let paragraph2 = "Paragraph 2 - Click to continue...";
+let paragraph3 = "Paragraph 3.";
+let paragraph4 = "Enter your name";
+let textBox = document.getElementById("textBox");
+document.getElementById("nameForm").style.display = "none";
+
+function nextParagraph() {
+  let textElement = document.getElementById("text");
+  //   document.getElementById("nameForm").style.display = "none";
+
+  if (textElement.textContent === paragraph1) {
+    textElement.textContent = paragraph2;
+  } else if (textElement.textContent === paragraph2) {
+    textElement.textContent = paragraph3;
+  } else if (textElement.textContent === paragraph3) {
+    textElement.textContent = paragraph4;
+    document.getElementById("nameForm").style.display = "block";
+  }
+
+  //else if (textElement.textContent === paragraph3) {
+  //     document.getElementById("text").style.display = "none";
+  //   }
+}
+
+textBox.addEventListener("click", nextParagraph);
