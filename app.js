22d7ca403f1b5d7a55e00842e6f86a488023c233
Jonathan OReilly <jonathanoreilly@live.co.uk>
2023-07-06 13:43:03 +0100

    finished stats form and making race form

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ let paragraph1 =
 let paragraph2 = "Paragraph 2 - Click to continue...";
 let paragraph3 = "Paragraph 3.";
 let paragraph4 = "Enter your name:";
+let paragraph5 = "To choose your s";
 let textBox = document.getElementById("textBox");
-let nameForm = document.getElementById("nameForm").value;
-document.getElementById("nameForm").style.display = "none";
-
+let nameForm = document.getElementById("nameForm");
+let statForm = document.getElementById("statForm");
+console.log(nameForm);
 function nextParagraph() {
   let textElement = document.getElementById("text");
-  //   document.getElementById("nameForm").style.display = "none";
 
   if (textElement.textContent === paragraph1) {
     textElement.textContent = paragraph2;
@@ -18,16 +18,35 @@ function nextParagraph() {
     textElement.textContent = paragraph3;
   } else if (textElement.textContent === paragraph3) {
     textElement.textContent = paragraph4;
+    textBox.removeEventListener("click", nextParagraph);
     document.getElementById("nameForm").style.display = "block";
   }
+}
 
-  //else if (textElement.textContent === paragraph3) {
-  //     document.getElementById("text").style.display = "none";
-  //   }
+function nameFunction(event) {
+  event.preventDefault();
+  let firstNameValue = event.target.firstName.value;
+  let lastNameValue = event.target.lastName.value;
+  console.log(firstNameValue + lastNameValue);
+  document.getElementById("nameForm").style.display = "none";
+  document.getElementById("statForm").style.display = "table";
+  //create a varible for firstname + last name called charName then store it in local storage
+}
+
+function statFunction(event) {
+  event.preventDefault();
+  let strValue = event.target.strength.value;
+  // let agiValue = event.target.agility.value;
+  let conValue = event.target.constitution.value;
+  let intValue = event.target.intelligence.value;
+  let wisValue = event.target.wisdom.value;
+  let chaValue = event.target.charisma.value;
+
+  document.getElementById("statForm").style.display = "none";
 }
 
 let firstName = document.getElementById("firstName");
-console.log(firstName);
 
 textBox.addEventListener("click", nextParagraph);
-console.log(nameForm);
+nameForm.addEventListener("submit", nameFunction);
+statForm.addEventListener("submit", statFunction);
